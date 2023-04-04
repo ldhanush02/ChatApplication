3a26e53299ca0212f5f28755e3dfbfc968a58289
gvineeth1552 <gvineeth1552@gmail.com>
2023-04-04 18:47:22 +0530

    Version 0.0

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Lokam Dhanush!</Text>
+      <Text>Lokam Dhanush! Roll No 33</Text>
       <StatusBar style="auto" />
     </View>
   );
