ed34712a1710b112ae02a6697cc63cdbe62ab3e4
saiteja18g <saitejagandla3@gmail.com>
2023-04-27 16:07:44 +0530

    input_box_comp

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,12 +1,16 @@
 // src/screens/ChatScreen.js
 
-import { ImageBackground, StyleSheet, FlatList } from "react-native";
+import { ImageBackground, StyleSheet, FlatList, KeyboardAvoidingView } from "react-native";
+import InputBox from "../Components/InputBox";
 import bg from "../../assets/images/BG.png";
 import Message from "../Components/Message";
 import messages from "../../assets/data/messages.json";
+// import { KeyboardAvoidingView } from "react-native-web";
+import { Platform } from "react-native";
 
 const ChatScreen = () => {
   return (
+    <KeyboardAvoidingView behavior={Platform.OS==='ios' ? 'padding' : 'height'} style={styles.bg}>
     <ImageBackground source={bg} style={styles.bg}>
       <FlatList
         data={messages}
@@ -14,7 +18,9 @@ const ChatScreen = () => {
 				style={{ padding: 12 }}
 				inverted
       />
+      <InputBox />
     </ImageBackground> 
+    </KeyboardAvoidingView>
   );
 };
 
