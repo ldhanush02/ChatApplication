80029da53249fb3813740202b05408fc3a811156
gvineeth1552 <gvineeth1552@gmail.com>
2023-05-06 22:56:23 +0530

    backend done

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Navigator from './src/navigation';
-import { Amplify } from 'aws-amplify';
+import { Amplify , Auth, API, graphqlOperation} from 'aws-amplify';
 import { withAuthenticator } from "aws-amplify-react-native" 
 import awsconfig from "./src/aws-exports"
+import { useEffect } from 'react';
+import { getUser } from './src/graphql/queries';
+import { createUser } from './src/graphql/mutations';
+
 //import ChatListItem from './src/Components/ChatListItem';
 
 Amplify.configure({...awsconfig, Analytics: {disabled:true}});
@@ -22,6 +26,54 @@ Amplify.configure({...awsconfig, Analytics: {disabled:true}});
 };*/
 
 function App() {
+
+  useEffect(() => {
+
+ const syncUser = async () => {
+
+  //get Auth user
+  const authUser = await Auth.currentAuthenticatedUser({
+    bypassCache: true,
+  }); 
+  console.log('1',authUser);
+
+  //query the database using Auth user id(sub)
+  console.log('6');
+  const userData = await API.graphql(
+    graphqlOperation(getUser, { id: authUser.attributes.sub})
+  );
+ 
+  console.log('5');
+  console.log('2',userData); 
+
+  if(userData.data.getUser){
+    console.log('3',"User already exists in DB");
+    return;
+  }
+
+  //IF THERE IS NO USER IN DB CREATE ONE
+
+  const newUser = {
+    id: authUser.attributes.sub,
+    name: authUser.attributes.phone_number,
+    status: "Hey , I am using Whatsapp",
+  };
+
+  console.log('4',newUser);
+
+    const newUserResponse = await API.graphql(
+
+    graphqlOperation(createUser, {input: newUser})
+
+  );
+ };
+ 
+ syncUser();
+
+  }, []);
+
+
+
   return (
     <View style={styles.container}>
       <Navigator/>
--- src/Components/ContactListItem/index.js
+++ src/Components/ContactListItem/index.js
@@ -1,26 +1,63 @@
 import { Text, Image, StyleSheet, Pressable, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { API, graphqlOperation , Auth} from 'aws-amplify';
+import { createChatRoom, createUserChatRoom } from '../../graphql/mutations';
+import {getCommonChatRoomWithUser} from '../../services/chatRoomService';
 
 import dayjs from 'dayjs';
 import relativeTime from "dayjs/plugin/relativeTime";
+
 dayjs.extend(relativeTime);
 
 const ContactListItem = ({ user }) => {
   const navigation = useNavigation();
 
+  const onPress = async () => {
+    //Check if we already have a ChatRoom with this user//
+    const existingChatRoom = await getCommonChatRoomWithUser(user.id);
+    if (existingChatRoom) {
+      navigation.navigate("Chat" , {id: existingChatRoom.id});
+      return;
+    }
+    // Create a chat room//
+    const newChatRoomData = await API.graphql(
+      graphqlOperation(createChatRoom, { input: {} })
+    );
+    console.log(newChatRoomData);
+    if (!newChatRoomData.data?.createChatRoom) {
+      console.log("ChatRoom not created");
+    }
+    const newChatRoom = newChatRoomData.data?.createChatRoom;
+
+    //Add the clicked user to the chat room//
+    await API.graphql(graphqlOperation(createUserChatRoom, {
+      input: { chatRoomId: newChatRoom.id, userId: user.id },
+    })
+    );
+    //Add the auth user to the ChatRoom//
+    const authUser = await Auth.currentAuthenticatedUser();
+    await API.graphql(
+      graphqlOperation(createUserChatRoom, {
+      input: { chatRoomId: newChatRoom.id, userId: authUser.attributes.sub },
+    })
+    );
+    //navigate to the newly created chat room//
+    navigation.navigate("Chat", { id: newChatRoom.id });
+  };
+
   return (
-    <Pressable onPress={() =>{}} style={styles.container}>
-      <Image source ={{ uri: user.image }} style={styles.image}/>
+    <Pressable onPress={onPress} style={styles.container}>
+      <Image source={{ uri: user.image }} style={styles.image} />
 
-        <View style={styles.content}>
-            <Text style={styles.name} numberOfLines={1}>
-                {user.name}
-            </Text> 
+      <View style={styles.content}>
+        <Text style={styles.name} numberOfLines={1}>
+          {user.name}
+        </Text>
 
-            <Text numberOfLines={2} style={styles.subTitle}>
-                {user.status}
-            </Text>
-        </View>
+        <Text numberOfLines={2} style={styles.subTitle}>
+          {user.status}
+        </Text>
+      </View>
     </Pressable>
   );
 };
